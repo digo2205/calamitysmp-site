207bb73d0ff2d951e4759c9c0bf1b524a5f79b61
Digo <digaomontechezi@gmail.com>
2025-10-08 01:44:02 -0300

    Correcao 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom' // <-- importa o HashRouter
 import App from './App.tsx'
 import './globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter> {/* <-- envolve o App com HashRouter */}
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
 )
